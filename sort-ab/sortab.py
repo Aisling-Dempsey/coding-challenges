3d9cdaecacf978ef10a0ee15dbdab0ef73919aae
Aisling Dempsey <aisling.n.dempsey@gmail.com>
2016-06-20 16:35:34 -0700

    sort-ab added and soved

---
+++ sort-ab/sortab.py
@@ -0,0 +1,44 @@
+"""Given already-sorted lists, `a` and `b`, return sorted list of both.
+
+You may not use sorted() or .sort().
+
+Check edge cases of empty lists:
+
+    >>> sort_ab([], [])
+    []
+
+    >>> sort_ab([1, 2,3], [])
+    [1, 2, 3]
+
+    >>> sort_ab([], [1, 2, 3])
+    [1, 2, 3]
+
+Check:
+
+    >>> sort_ab([1, 3, 5, 7], [2, 6, 8, 10])
+    [1, 2, 3, 5, 6, 7, 8, 10]
+"""
+
+
+def sort_ab(a, b):
+    """Given already-sorted lists, `a` and `b`, return sorted list of both.
+
+    You may not use sorted() or .sort().
+    """
+    s_list = []
+    while a and b:
+        if a[0] < b[0]:
+            s_list.append(a.pop(0))
+        elif b[0] < a[0]:
+            s_list.append(b.pop(0))
+
+    if len(a) == 0:
+        s_list = s_list + b
+    elif len(b) == 0:
+        s_list = s_list + a
+
+    return s_list
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. YOU'RE A MERGE CHAMPION!!\n"
