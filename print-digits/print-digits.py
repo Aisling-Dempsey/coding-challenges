b63e4ce0a0638468f39b50c603f6a4ce57d8666e
Aisling Dempsey <aisling.n.dempsey@gmail.com>
2016-06-20 11:55:58 -0700

    print-digits added and completed

---
+++ print-digits/print-digits.py
@@ -0,0 +1,38 @@
+"""Given int, print each digit in reverse order, starting with the ones place.
+
+For example::
+
+    >>> print_digits(1)
+    1
+
+    >>> print_digits(413)
+    3
+    1
+    4
+
+    >>> print_digits(7331)
+    1
+    3
+    3
+    7
+
+"""
+
+
+def print_digits(num):
+    """Given int, print digits in reverse order, starting with the ones place."""
+    # print 'in function'
+        # print "in the while loop"
+    remainder = num % 10
+    print remainder
+    new_num = (num - remainder) / 10
+    # print num
+    if new_num > 0:
+        print_digits(new_num)
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. WOWZA!\n"
